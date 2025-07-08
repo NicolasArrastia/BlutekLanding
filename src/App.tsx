dfdcef71d80c88dac33a0321479e41f2a87e36ff
Nicolas <nicolasarrastia@gmail.com>
2025-07-08 02:35:43 -0300

    add base sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import Header from "./components/Header";
 import AboutUsSection from "./sections/AboutUsSection";
+import ContactSection from "./sections/ContactSection";
 import HeroSection from "./sections/HeroSection";
-import ProjectsSection from "./sections/ProjectsSection";
+import PortfolioSection from "./sections/PortfolioSection";
+import ProcessSection from "./sections/ProcessSection";
+import ServicesSection from "./sections/ServicesSection";
 
 const App = () => {
   return (
-    <>
+    <div className="[&>section]:flex [&>section]:flex-col [&>section]:items-center [&>section]:py-20 [&>section]:px-5">
       <Header />
       <HeroSection />
       <AboutUsSection />
-      <ProjectsSection />
-    </>
+      <ServicesSection />
+      <ProcessSection />
+      <PortfolioSection />
+      <ContactSection />
+    </div>
   );
 };
 
