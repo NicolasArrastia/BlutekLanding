b6c597135b46fa09af9e9cad134bd373d403f68b
Nicolas <nicolasarrastia@gmail.com>
2025-07-09 21:08:53 -0300

    Remove api

--- api/src/routes/resendRouter.js
+++
@@ -1,7 +0,0 @@
-import {Router} from "express";
-import { sendEmail } from "../controller/resendController.js";
-const router = Router()
-router.post("/send-email", sendEmail)
-
-
-export {router as resendRouter}; 
